bc1982a90518ae84c6e58fa9f3d6f2e90cada90e
narmathagn <42265938+narmathagn@users.noreply.github.com>
2026-01-07 17:42:49 +0530

    Add files via upload

---
+++ env.js
@@ -0,0 +1,5 @@
+// This file is ignored by Git to protect your sensitive URL.
+// It also works locally via file:/// unlike .env files.
+const ENV = {
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwoUbhH6D6QIZP1coK3LgypD2tlQiAJCapKafeGerc6j690tyzKG_wVCz9CdR_iIqYP/exec'
+};
